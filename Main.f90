3d03390ee696f7c367d81994d2b7fcfaaeae1006
fussionman26 <galway1212@hotmail.com>
2016-08-10 18:12:48 -0400

    Fixed issue of time step with Igor

--- Main.f90
+++ Main.f90
@@ -2,12 +2,12 @@
 module constants
 implicit none
 real,parameter    :: gamma    = 5./3.,&
-					 Courant  = .9  ,&
+					 Courant  = .5  ,&
 					 length   = 1.  ,&
 					 tmax     = 10. ,&
-					 qo       = 2.  ,&  ! 
+					 qo       = 5.  ,&  
 					 delta    = 1.e-30 ,&
-					 uL       = .5 ,&      !(uL=.5 for piston , uL=.0 for shock tube)
+					 uL       = 10. ,&      !(uL=.5 for piston , uL=.0 for shock tube)
 					 uR       = 1.e-30  	 !(uR=.0 for piston , uR=.0 for shock tube)
 integer,parameter :: jmax 	  = 100  ,& ! number of cells
 					 itterMax = 1000  ,& ! Maximum itterations
@@ -18,7 +18,7 @@ end module
 program main 
 use constants
 implicit none 
-real :: dt=0.05 , time=.0  , dmi
+real :: dt=0.00005 , time=.0  , dmi
 real,dimension(0:jmax+1) :: rho,p,e,q,c,dx,dm,r
 real,dimension(-1:jmax+1) :: u,x,uT
 integer :: i=0 , j
@@ -29,34 +29,24 @@ call InitCond(rho,e,p,c,q,u,dm,dx,x)
 
 do while (time.LT.tmax.AND.i.LT.itterMax)
 	call Boundary(u)
-	call Timestep(u,dx,dt,c,i)
-	!dt=0.05
 	call Output(rho,e,p,u,dm,time,x,r,i)
 	x(-1)  = x(-1) + dt*u(-1)
 	do j=0,jmax  
-		!!dmi = .25*(rho(j+1)+rho(j))*(x(j+1)-x(j-1))
-		dmi    = .5*(rho(j+1)+rho(j))*(r(j+1)-r(j))    ! Mass in "momentum zone" of cell interface of j and j+1 cell
 		x(j)   = x(j) + u(j)*dt
-		uT(j)  = u(j) + (dt/dmi)*(p(j)+q(j)-p(j+1)-q(j+1)) ! Update the velocity in a temporary array
+		dmi    = .5*(rho(j+1)+rho(j))*(r(j+1)-r(j))    ! Mass in "momentum zone" of cell interface of j and j+1 cell
+		q(j) = qo*(dm(j)/(x(j)-x(j-1)))*(MIN(0.0,u(j)-u(j-1)))**2
+		u(j)  = u(j) + (dt/dmi)*(p(j)+q(j)-p(j+1)-q(j+1)) ! Update the velocity in a temporary array
 		rho(j) = dm(j)/(x(j)-x(j-1))
 		e(j)   = e(j) + (dt/dm(j))*(p(j)+q(j))*(u(j-1)-u(j))
 		p(j)   = (gamma-1.)*rho(j)*e(j)
-		if (uT(j-1)-uT(j).GT..0) then  ! If updated cell is compressed add viscous pressure
-			q(j) = qo*rho(j)*(uT(j-1)-uT(j))**(2)
-			c(j)   = sqrt(gamma*(p(j)+q(j))/rho(j))
-			!c(j) = sqrt(gamma*(p(j)+q(j))/rho(j))+uL
-			!q(j)   = qo*rho(j)*c(j)*(uT(j-1)-uT(j))
-		else 
-			q(j) = .0
-			c(j)   = sqrt(gamma*p(j)/rho(j))
-		end if 
-		
+
 	end do 
-	u(0:jmax) = uT (0:jmax)		     ! Update the velocity 
 	r   = .5*(x(0:jmax+1)+x(-1:jmax))! Find new cell centers
 	dx  =  x(0:jmax+1)-x(-1:jmax)   ! Find new cell widths
 	i = i + 1						! Update itteration counter
 	time = time+ dt					! update timer
+	call Output(rho,e,p,u,dm,time,x,r,i)
+	call Timestep(u,dx,dt,c,i)
 	if (MINVAL(dx).LT..0) then
 		print*,'Negative volume'
 		exit 
@@ -99,10 +89,10 @@ integer :: i
 
 if (IC.EQ.1) then
 	rho = 1.
-	p   = delta
+	p   = 1.
 	e   = p/((gamma-1.)*rho)
 	u(-1)        = uL     ! Required to be equal to the piston speed on left (moving)
-	u(0:jmax)    = delta
+	u(0:jmax)    = 0.
 	u(jmax+1)    = uR     ! Required to be equal to the piston speed on right (stationary)
 else if (IC.EQ.2) then
 	do i=0,jmax+1
@@ -123,7 +113,7 @@ end if
 do i=0,jmax+1
 	if (u(i-1)-u(i).GT..0) then
 		q(i) = qo*rho(i)*(u(i-1)-u(i))**(2)
-		c(i) = sqrt(gamma*(p(i)+q(i))/rho(i))
+		c(i) = sqrt(gamma*(p(i))/rho(i))
 		!c(i) = sqrt(gamma*p(i)/rho(i)) + uL
 		!q(i)   = qo*rho(i)*c(i)*(u(i-1)-u(i))
 	else 
@@ -144,9 +134,9 @@ real,intent(in),dimension(0:jmax+1)  :: dx,c
 integer,intent(in) :: i
 real :: dt_temp 
 dt_temp = dt                            ! Previous time step
-dt = COURANT*MINVAL(dx)/(MAXVAL(ABS(u(-1:jmax))+c))
+dt = COURANT*MINVAL(dx/(ABS(.5*(u(-1:jmax)+u(0:jmax+1)))+c))
 if (i.NE.0) then
-	dt = MIN(dt,2.*dt_temp)  				   ! Doesnt allow time step to grow too fast
+	dt = MIN(dt,1.1*dt_temp)  				   ! Doesnt allow time step to grow too fast
 end if 
 end subroutine 
 
@@ -157,7 +147,7 @@ real,intent(in),dimension(0:jmax+1)   :: d,e,p,dm,r
 real,intent(in),dimension(-1:jmax+1) :: x,u
 integer,intent(in) :: itter
 real,intent(in) :: time
-integer :: st,en,choice 
+integer :: st,en,choice ,j
 choice = 0 ! Set choice to 0 to prin out real domain, 1 to print out entire domain (including ghost cells)
 
 st = 1
@@ -165,11 +155,13 @@ en = jmax
 
 
 if (MOD(itter,writeStep)==0) then ! Only write out itter modulus writeStep (speeds up plotting)
-    write(1,*) d(1:jmax) ! Rho 
-    write(2,*) u(0:jmax) ! Velocity
+    !write(1,"(1D12.5)") d(1:jmax) ! Rho 
+    write(1,*) d(1:jmax)
+	write(2,*) u(0:jmax) ! Velocity
     write(3,*) e(1:jmax) ! Internal Energy
     write(4,*) p(1:jmax) ! Pressure
-	write(7,*) x(0:jmax) ! interface
+	write(7,*) x(0:jmax)
+	!write(7,"(6(1PD12.5))") (u(j),(dt),(p(j)+q(j)-p(j+1)-q(j+1)),q(j),dm(j),x(j) ,j=-1,jmax)  ! interface
 	write(8,*) time      ! Current Time
     write(9,*) r(1:jmax) ! Lagrange Cell Ceters
     write(10,*) SUM(dm)               ! Total mass
